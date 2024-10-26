7ccddbe72c238c4204e60f5ed898929e2da8ca33
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2024-10-26 15:24:01 +0530

    morphtext removed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import TextReveal from './UltimatePortfolio/newComponent/components/TextReveal';
-import MorphingText from './UltimatePortfolio/newComponent/sections/morphText';
 
 const App = () => {
   return (
