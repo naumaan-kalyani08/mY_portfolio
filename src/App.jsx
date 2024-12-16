625560d4d9d7b75ba9b30b2a0530b1c0ffa2e30a
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2024-12-17 02:40:23 +0530

    responsive css added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import TextReveal from './UltimatePortfolio/newComponent/components/TextReveal';
 import HorizontalScroll from './UltimatePortfolio/newComponent/components/HorizontalScroll';
 import PageLoader from './UltimatePortfolio/newComponent/components/PageLoader';
-
+import'./UltimatePortfolio/newComponent/style/Responsive.css'
 const App = () => {
   return (
     <>
