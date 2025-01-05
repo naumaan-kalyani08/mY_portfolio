a1be254c557ad4b14d804e75f827f55e49d957e4
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-01-05 14:32:41 +0530

    es-lint fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import TextReveal from './UltimatePortfolio/newComponent/components/TextReveal';
+// import TextReveal from './UltimatePortfolio/newComponent/components/TextReveal';
 import HorizontalScroll from './UltimatePortfolio/newComponent/components/HorizontalScroll';
 import PageLoader from './UltimatePortfolio/newComponent/components/PageLoader';
 import'./UltimatePortfolio/newComponent/style/Responsive.css'
