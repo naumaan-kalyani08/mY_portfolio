b903b9d2ee6e5324d4f1b9d1c6cd1981a2002767
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-01-12 22:03:50 +0530

    alt image added

--- src/UltimatePortfolio/newComponent/sections/PhotoGrid.jsx
+++ src/UltimatePortfolio/newComponent/sections/PhotoGrid.jsx
@@ -8,135 +8,146 @@ const PhotoGrid = () => {
     <section>
       <div className="container">
         <SectionButton btnText="Best Moments" />
-        <p className="text-center text-white"> press on any image to have a better view </p>
+        <p className="text-center text-white">
+          {" "}
+          press on any image to have a better view{" "}
+        </p>
         <div className="masonry-grid">
-        <Gallery>
-          <Item
-            original="http://localhost:3000/images/heroSlider/hero-1.jpg"
-            thumbnail="http://localhost:3000/images/heroSlider/hero-1.jpg"
-            width="400"
-            height="600"
-          >
-            {({ ref, open }) => (
-              <img
-                ref={ref}
-                width={300}
-                onClick={open}
-                src="http://localhost:3000/images/heroSlider/hero-1.jpg"
-              />
-            )}
-          </Item>
-          <Item
-            original="http://localhost:3000/images/heroSlider/hero-02.jpg"
-            thumbnail="http://localhost:3000/images/heroSlider/hero-02.jpg"
-            width="400"
-            height="600"
-          >
-            {({ ref, open }) => (
-              <img
-                ref={ref}
-                width={300}
-                onClick={open}
-                src="http://localhost:3000/images/heroSlider/hero-02.jpg"
-              />
-            )}
-          </Item>
+          <Gallery>
+            <Item
+              original="./images/heroSlider/hero-1.jpg"
+              thumbnail="./images/heroSlider/hero-1.jpg"
+              width="400"
+              height="600"
+            >
+              {({ ref, open }) => (
+                <img
+                  alt="photo-grid"
+                  ref={ref}
+                  width={300}
+                  onClick={open}
+                  src="./images/heroSlider/hero-1.jpg"
+                />
+              )}
+            </Item>
+            <Item
+              original="./images/heroSlider/hero-02.jpg"
+              thumbnail="./images/heroSlider/hero-02.jpg"
+              width="400"
+              height="600"
+            >
+              {({ ref, open }) => (
+                <img
+                  alt="photo-grid"
+                  ref={ref}
+                  width={300}
+                  onClick={open}
+                  src="./images/heroSlider/hero-02.jpg"
+                />
+              )}
+            </Item>
 
-          <Item
-            original="http://localhost:3000/images/heroSlider/hero-1.jpg"
-            thumbnail="http://localhost:3000/images/heroSlider/hero-1.jpg"
-            width="400"
-            height="600"
-          >
-            {({ ref, open }) => (
-              <img
-                ref={ref}
-                width={300}
-                onClick={open}
-                src="http://localhost:3000/images/heroSlider/hero-1.jpg"
-              />
-            )}
-          </Item>
+            <Item
+              original="./images/heroSlider/hero-1.jpg"
+              thumbnail="./images/heroSlider/hero-1.jpg"
+              width="400"
+              height="600"
+            >
+              {({ ref, open }) => (
+                <img
+                  alt="photo-grid"
+                  ref={ref}
+                  width={300}
+                  onClick={open}
+                  src="./images/heroSlider/hero-1.jpg"
+                />
+              )}
+            </Item>
 
-          <Item
-            original="http://localhost:3000/images/heroSlider/hero-02.jpg"
-            thumbnail="http://localhost:3000/images/heroSlider/hero-02.jpg"
-            width="400"
-            height="600"
-          >
-            {({ ref, open }) => (
-              <img
-                ref={ref}
-                width={300}
-                onClick={open}
-                src="http://localhost:3000/images/heroSlider/hero-02.jpg"
-              />
-            )}
-          </Item>
+            <Item
+              original="./images/heroSlider/hero-02.jpg"
+              thumbnail="./images/heroSlider/hero-02.jpg"
+              width="400"
+              height="600"
+            >
+              {({ ref, open }) => (
+                <img
+                  alt="photo-grid"
+                  ref={ref}
+                  width={300}
+                  onClick={open}
+                  src="./images/heroSlider/hero-02.jpg"
+                />
+              )}
+            </Item>
 
-          <Item
-            original="http://localhost:3000/images/heroSlider/hero-1.jpg"
-            thumbnail="http://localhost:3000/images/heroSlider/hero-1.jpg"
-            width="400"
-            height="600"
-          >
-            {({ ref, open }) => (
-              <img
-                ref={ref}
-                width={300}
-                onClick={open}
-                src="http://localhost:3000/images/heroSlider/hero-1.jpg"
-              />
-            )}
-          </Item>
-          <Item
-            original="http://localhost:3000/images/heroSlider/hero-02.jpg"
-            thumbnail="http://localhost:3000/images/heroSlider/hero-02.jpg"
-            width="400"
-            height="600"
-          >
-            {({ ref, open }) => (
-              <img
-                ref={ref}
-                width={300}
-                onClick={open}
-                src="http://localhost:3000/images/heroSlider/hero-02.jpg"
-              />
-            )}
-          </Item>
+            <Item
+              original="./images/heroSlider/hero-1.jpg"
+              thumbnail="./images/heroSlider/hero-1.jpg"
+              width="400"
+              height="600"
+            >
+              {({ ref, open }) => (
+                <img
+                  alt="photo-grid"
+                  ref={ref}
+                  width={300}
+                  onClick={open}
+                  src="./images/heroSlider/hero-1.jpg"
+                />
+              )}
+            </Item>
+            <Item
+              original="./images/heroSlider/hero-02.jpg"
+              thumbnail="./images/heroSlider/hero-02.jpg"
+              width="400"
+              height="600"
+            >
+              {({ ref, open }) => (
+                <img
+                  alt="photo-grid"
+                  ref={ref}
+                  width={300}
+                  onClick={open}
+                  src="./images/heroSlider/hero-02.jpg"
+                />
+              )}
+            </Item>
 
-          <Item
-            original="http://localhost:3000/images/heroSlider/hero-1.jpg"
-            thumbnail="http://localhost:3000/images/heroSlider/hero-1.jpg"
-            width="400"
-            height="600"
-          >
-            {({ ref, open }) => (
-              <img
-                ref={ref}
-                width={300}
-                onClick={open}
-                src="http://localhost:3000/images/heroSlider/hero-1.jpg"
-              />
-            )}
-          </Item>
+            <Item
+              original="./images/heroSlider/hero-1.jpg"
+              thumbnail="./images/heroSlider/hero-1.jpg"
+              width="400"
+              height="600"
+            >
+              {({ ref, open }) => (
+                <img
+                  alt="photo-grid"
+                  ref={ref}
+                  width={300}
+                  onClick={open}
+                  src="./images/heroSlider/hero-1.jpg"
+                />
+              )}
+            </Item>
 
-          <Item
-            original="http://localhost:3000/images/heroSlider/hero-02.jpg"
-            thumbnail="http://localhost:3000/images/heroSlider/hero-02.jpg"
-            width="400"
-            height="600"
-          >
-            {({ ref, open }) => (
-              <img
-                ref={ref}
-                width={300}
-                onClick={open}
-                src="http://localhost:3000/images/heroSlider/hero-02.jpg"
-              />
-            )}
-          </Item>
-        </Gallery>
+            <Item
+              original="./images/heroSlider/hero-02.jpg"
+              thumbnail="./images/heroSlider/hero-02.jpg"
+              width="400"
+              height="600"
+            >
+              {({ ref, open }) => (
+                <img
+                  alt="photo-grid"
+                  ref={ref}
+                  width={300}
+                  onClick={open}
+                  src="./images/heroSlider/hero-02.jpg"
+                />
+              )}
+            </Item>
+          </Gallery>
         </div>
       </div>
     </section>
