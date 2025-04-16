9b7c96badf398c52c4245e03f1c960eb02f08069
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-04-17 01:49:13 +0530

    updated pramaters and component added

--- src/UltimatePortfolio/newComponent/components/ReusableComponents.jsx
+++ src/UltimatePortfolio/newComponent/components/ReusableComponents.jsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-
-const ReusableComponents = ({additionalStyle}) => {
+const ReusableComponents = ({ additionalStyle }) => {
   return (
     <div className={`first-scroll-icon ${additionalStyle}`}>
-    <div className="mouse"> </div>
-  </div>
-  )
-}
-
-const ScrollAlert = ({additionalStyle}) => {
-  return (
-    <div className={`first-scroll-icon ${additionalStyle}`}>
-    <div className="mouse"> </div>
-      <p className='mt-1 highlight'> scroll down to continue </p>
-  </div>
-  )
-}
-
-const SectionButton = ({btnText}) =>{
-  return(
-    <div className="d-flex align-items-center justify-content-center about-btn-wrapper py-3">
-    <div className="button-wrapper">
-      <button
-        className="button"
-        type="button"
-        data-aos="fade-right "
-        data-aos-duration="3000"
-      >
-        {btnText}
-      </button>
-      <div className="button-bg"></div>
+      <div className="mouse"> </div>
     </div>
-  </div>
-  )
-}
+  );
+};
+
+const ScrollAlert = ({ additionalStyle }) => {
+  return (
+    <div className={`first-scroll-icon ${additionalStyle}`}>
+      <div className="mouse"> </div>
+      <p className="mt-1 highlight"> scroll down to continue </p>
+    </div>
+  );
+};
+
+const SectionButton = ({ btnText, onClick }) => {
+  return (
+    <div className="d-flex align-items-center justify-content-center about-btn-wrapper py-3">
+      <div className="button-wrapper">
+        <button
+          className="button"
+          type="button"
+          data-aos="fade-right "
+          data-aos-duration="3000"
+          onClick={onClick}
+        >
+          {btnText}
+        </button>
+        <div className="button-bg"></div>
+      </div>
+    </div>
+  );
+};
 
 ReusableComponents.propTypes = {
   additionalStyle: PropTypes.string,
@@ -47,6 +47,7 @@ ScrollAlert.propTypes = {
 
 SectionButton.propTypes = {
   btnText: PropTypes.string,
+  onClick: PropTypes.func,
 };
 
-export {ReusableComponents , ScrollAlert ,SectionButton}
+export { ReusableComponents, ScrollAlert, SectionButton };
