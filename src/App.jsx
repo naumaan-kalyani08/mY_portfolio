40afeddec98acc5805bc9a3fe5f97ae27728a3e1
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-04-27 19:47:06 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import HorizontalScroll from "./UltimatePortfolio/newComponent/components/Horizo
 import PageLoader from "./UltimatePortfolio/newComponent/components/PageLoader";
 import "./UltimatePortfolio/newComponent/style/theme.css";
 import "../src/UltimatePortfolio/newComponent/style/Responsive.css";
+// import "./UltimatePortfolio/newComponent/style/ScrollPathAnimation.css";
 import About from "./UltimatePortfolio/newComponent/sections/Testing";
 import "../src/UltimatePortfolio/newComponent/style/override.css";
 import TechStack from "./UltimatePortfolio/newComponent/sections/TechStack";
 import ContactUs from "./UltimatePortfolio/newComponent/sections/ContactUs";
+// import BeePathAnimation from "./UltimatePortfolio/newComponent/components/ProfessionalJourney";
 // import Timeline from './UltimatePortfolio/newComponent/sections/TimelineSection';
 // import PhotoGrid from './UltimatePortfolio/newComponent/sections/PhotoGrid';
 const App = () => {
@@ -21,6 +23,7 @@ const App = () => {
       {/* <Timeline/> */}
       {/* <TextReveal/>      */}
       {/* <PhotoGrid/> */}
+      {/* <BeePathAnimation /> */}
       <TechStack />
       <ContactUs />
     </>
