4a2e3250130a6f21c38d7d41c71e356d80b0f7aa
naumaan-kalyani08 <naumaan.kalyani02@gmail.com>
2025-04-29 23:09:11 +0530

    daily commit

--- src/UltimatePortfolio/newComponent/components/ProfessionalJourney.jsx
+++ src/UltimatePortfolio/newComponent/components/ProfessionalJourney.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useLayoutEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-import "../style/ScrollPathAnimation.css";
 
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 
@@ -39,8 +38,8 @@ const BeePathAnimation = () => {
         ease: "none",
         scrollTrigger: {
           trigger: world, // scroll starts when BeePathAnimation section is in view
-          start: "top center", // animation starts when 'world' reaches center of viewport
-          end: "bottom center",
+          start: "top bottom", // animation starts when 'world' reaches the bottom of the viewport
+          end: "bottom center", // animation ends when 'world' reaches the center of the viewport
           scrub: true,
           markers: true, // turn off after debugging
         },
@@ -59,60 +58,59 @@ const BeePathAnimation = () => {
   }, []);
 
   return (
-    <div className="custom-wrapper position-relative">
-      <div className="scrollpath-wrapper">
-        <div className="spacer" />
-        <section
-          className="container my-5 position-relative d-flex flex-column"
-          ref={worldRef}
-          id="world"
-        >
-          <svg
-            ref={svgRef}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 703 2467"
-            fill="none"
-            preserveAspectRatio="xMidYMin meet"
-            className="svg-path"
-          >
-            <path
-              ref={pathRef}
-              id="path1"
-              d="M2 2C2 196.5 484 160.5 596.5 438C709 715.5 33 1062 105.5 1301C204.5 1568 835 1736.5 675 2037C607.333 2164.09 393 2269 393 2465"
-              stroke="#231F20"
-              strokeWidth="4"
-              strokeLinecap="round"
-            />
-          </svg>
+    <section>
+      <div className="custom-wrapper position-relative">
+        <div className="scrollpath-wrapper">
+          <div className="spacer" />
+          <section className="" ref={worldRef} id="world">
+            <svg
+              ref={svgRef}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 703 2467"
+              fill="none"
+              preserveAspectRatio="xMidYMin meet"
+              className="svg-path"
+            >
+              <path
+                ref={pathRef}
+                id="path1"
+                d="M2 2C2 196.5 484 160.5 596.5 438C709 715.5 33 1062 105.5 1301C204.5 1568 835 1736.5 675 2037C607.333 2164.09 393 2269 393 2465"
+                stroke="#231F20"
+                strokeWidth="4"
+                strokeLinecap="round"
+              />
+            </svg>
 
-          <div className="gap-5">
-            {[...Array(5)].map((_, index) => (
-              <div className="col-md-6 offset-md-2" key={index}>
-                <div
-                  className="box"
-                  ref={(el) => (boxRefs.current[index] = el)}
-                />
+            <div className="gap-5">
+              {[...Array(5)].map((_, index) => (
+                // <div className="col-md-6 offset-md-2" key={index}>
+                <div className="" key={index}>
+                  <div
+                    className="box"
+                    ref={(el) => (boxRefs.current[index] = el)}
+                  />
+                </div>
+              ))}
+            </div>
+
+            <div className="text-wrap">
+              <div className="content-right text-1">
+                <p>text 1</p>
               </div>
-            ))}
-          </div>
-
-          <div className="text-wrap">
-            <div className="content-right text-1">
-              <p>text 1</p>
+              <div className="conten-left text-2">
+                <p>text 2</p>
+              </div>
+              <div className="content-right text-3">
+                <p>text 3</p>
+              </div>
+              <div className="conten-left text-4">
+                <p>text 4</p>
+              </div>
             </div>
-            <div className="conten-left text-2">
-              <p>text 2</p>
-            </div>
-            <div className="content-right text-3">
-              <p>text 3</p>
-            </div>
-            <div className="conten-left text-4">
-              <p>text 4</p>
-            </div>
-          </div>
-        </section>
+          </section>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
